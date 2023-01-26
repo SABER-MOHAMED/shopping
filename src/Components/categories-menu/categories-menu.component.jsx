fba1c7ad2248fe432824c6d51e74faf724a86486
mohamed saber <mohamed.saber@edu.uiz.ac.ma>
2023-01-26 01:11:43 +0100

    Update: add categories Component

---
+++ src/Components/categories-menu/categories-menu.component.jsx
@@ -0,0 +1,41 @@
+import "./categories-menu.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const CategoriesMenu = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesMenu;
