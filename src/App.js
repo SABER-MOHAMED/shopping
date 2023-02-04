70ddb15adbec8b0645a0680e5bf52455a7f8a1e3
mohamed saber <mohamed.saber@edu.uiz.ac.ma>
2023-02-04 04:15:59 +0100

    small update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "./utils/firebase/firebase.utils";
+
 import Header from "./routes/header/header.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
@@ -5,8 +13,22 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      user && createUserDocumentFromAuth(user);
+
+      dispatch(setCurrentUser(user));
+
+      // console.log(dispatch(setCurrentUser(user)));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Header />}>
--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/* <UserProvider> */}
         <ProductsProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </ProductsProvider>
-      </UserProvider>
-    </BrowserRouter>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
